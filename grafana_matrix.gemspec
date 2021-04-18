7e634c57355dabed7e78fc92133d682176b3f5fb
Alexander Olofsson <ace@haxalot.com>
2021-04-18 21:04:52 +0200

    Add mocha and simplecov

--- grafana_matrix.gemspec
+++ grafana_matrix.gemspec
@@ -23,6 +23,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler'
   spec.add_development_dependency 'minitest'
+  spec.add_development_dependency 'mocha'
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'simplecov'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/vendor/'
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'grafana_matrix'
 
 require 'minitest/autorun'
+require 'mocha'
 
 def load_fixture(filename)
   File.open(File.join('test', 'fixtures', filename))
